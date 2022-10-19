4fa3a76dc58a55d9bf4f65a26166a2cbed0951b2
LeninMatos <leninmatos@gmail.com>
2022-10-19 18:15:09 -0300

    modified:   atualizando-lista.js e criado  juntando-listas  lista-duas-dimensoes

--- atualizando-lista.js
+++ atualizando-lista.js
@@ -1,7 +1,7 @@
 const listsaDeChamada =  [`João`, `Juliana`, `Ana`, `Caio`, `Laura`, `Marjorie`, `Guilherme`, `Aline`, `Fabiana`, `André`, `Carlos`, `Paulo`, `Bia`, `Vivian`, `Isabela`, `Vinivius`, `Renan`, `Renata`, `Daisy`, `Camilo`]
 
-// listsaDeChamada.splice(2,2,`Rodrigo`)
+// listsaDeChamada.splice(2,2,`Rodrigo`) //Removendo e adicionando
 
-listsaDeChamada.splice(3, 0, `Rodrigo`)
+listsaDeChamada.splice(3, 0, `Rodrigo`) //Sem remover ngm e só adicioando
 
 console.log(`Lista de chamada: ${listsaDeChamada}`)
